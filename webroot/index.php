723bf5579cd9bfb63d1cefe68334836278cfdde1
andrey <volosgoto@yahoo.com>
2017-08-02 12:27:33 +0300

    init

---
+++ webroot/index.php
@@ -0,0 +1,12 @@
+<?php 
+    // Определяем основные константы
+define ('DS', DIRECTORY_SEPARATOR);
+define ('ROOT', dirname(dirname(__FILE__)));
+
+$uri = $_SERVER['REQUEST_URI'];
+   
+echo 'Главня страница' . '<br>';
+print_r($uri);
+
+
+?>
